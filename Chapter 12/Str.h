581c287597ffa87c3e7fe077646889e5859f87f1
Owen <ohowland@gmail.com>
2017-02-15 16:42:37 -0700

    coffee

--- Chapter 12/Str.h
+++ Chapter 12/Str.h
@@ -1,12 +1,21 @@
 #ifndef GUARD_STR_H_
 #define GUARD_STR_H_
 
+#include <iostream>
+#include <cstddef>
 #include "Vec.h"
 
 class Str {
+    friend std::istream& operator>>(std::istream&, Str&);
+
 public:
+    Str& operator+=(const Str& s) {
+        std::copy(s.data.begin(), s.data.end(), std::back_inserter(data));
+        return *this;
+    }
+
     typedef Vec<char>::size_type size_type;
-    
+
     // Default Constructor
     Str() { } 
 
@@ -15,7 +24,7 @@ public:
 
     // Construct Str from null-terminated array of char
     Str(const char* cp) {
-        std::copy(cp, cp + std::strlen(cp), std::back_inserter(data));
+        std::copy(cp, cp + strlen(cp), std::back_inserter(data));
     }
 
     // Construct Str from the range denoted by iterators
@@ -24,8 +33,36 @@ public:
         std::copy(b, e, std::back_inserter(data));
     }
 
+    char& operator[](size_type i) { return data[i]; }
+    const char& operator[](size_type i) const { return data[i]; }
+    size_type size() const { return data.size(); }
+
 private:
     Vec<char> data;
+};
+
+std::ostream& operator<<(std::ostream& os, const Str& s)
+{
+    for (Str::size_type i = 0; i != s.size(); ++i)
+        os << s[i];
+    return os;
+}
+
+std::istream& operator>>(std::istream& is, Str& s)
+{
+    s.data.clear();
+    
+    char c;
+    while(is.get(c) && isspace(c))
+        ;
+    if (is) {
+        do s.data.push_back(c);
+        while (is.get(c) && !isspace(c));
+
+        if (is)
+            is.unget();
+    }
+    return is;
 }
 
 #endif
